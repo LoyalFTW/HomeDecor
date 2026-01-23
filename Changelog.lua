596bd4346874e98cacc4f10ef4491e20acb1eae6
Azro <47503034+LoyalFTW@users.noreply.github.com>
2026-01-23 09:45:48 -0400

    Change log updated

---
+++ Changelog.lua
@@ -0,0 +1,13 @@
+HomeDecor_Changelog = [[
+# HomeDecor
+
+## [v12.0.10](https://github.com/LoyalFTW/HomeDecor/tree/v12.0.10) (2026-01-22)
+[Full Changelog](https://github.com/LoyalFTW/HomeDecor/compare/v12.0.09...v12.0.10) [Previous Releases](https://github.com/LoyalFTW/HomeDecor/releases)
+
+- Updated missing Achievements  
+    Added missing Achievements items  
+    * Ebon Blade Planning Map  
+    * Fel Hammer Scouting Map  
+    * Glorious Pendant of Rukhmar - Added for Midnight 12.0 should be working now.  
+
+]]
