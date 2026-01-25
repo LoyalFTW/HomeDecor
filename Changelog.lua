5f223fe5f92134bf7d844b319706c67f2536cdbe
Azro <47503034+LoyalFTW@users.noreply.github.com>
2026-01-25 02:45:04 -0400

    Decor Items
    Added some missing Decor
    * Cragthorn Highlands Large Garden Cluster
    * Razorwind Blooms Large Garde
    * Razorwind Cactus Large Garden Cluster
    * Granite Cobblestone Path
    * Granite Cobblestone Path Arc
    * Granite Cobblestone Long Path
    * Granite Cobblestone Path Corner
    * Saltfang Shoals Large Garden Cluster

--- Changelog.lua
+++ Changelog.lua
@@ -1,18 +1,15 @@
 HomeDecor_Changelog = [[
 #HomeDecor
 
-What's New button added,
-you can enable it or disable it
-Its now in the bottom left
-
-Added missing quests (added new ones as well as added their vendors
-* Candle-Festooned Wooden Awning
-* Kobold Candle Trio
-* Kobold Digger's Chair
-* Kobold Wagon
-* Driftwood Barrel
-* Driftwood Junk Pile
-* Murloc's Wind Chimes
-* Writings of Reshad the Outcast
+Jan 25, 2026
+Added some missing Decor
+* Cragthorn Highlands Large Garden Cluster
+* Razorwind Blooms Large Garde
+* Razorwind Cactus Large Garden Cluster
+* Granite Cobblestone Path
+* Granite Cobblestone Path Arc
+* Granite Cobblestone Long Path
+* Granite Cobblestone Path Corner
+* Saltfang Shoals Large Garden Cluster
 
 ]]
