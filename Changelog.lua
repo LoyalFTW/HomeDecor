53985b3a0811d153951587f825c97698308aab88
Azro <47503034+LoyalFTW@users.noreply.github.com>
2026-03-02 17:51:54 -0400

    Update Changelog.lua

--- Changelog.lua
+++ Changelog.lua
@@ -1,6 +1,11 @@
 HomeDecor_Changelog = [[
 #HomeDecor
 
+March 2, 2026
+
+* Sales panel had an error not showing 
+the correct Id it now works and is tracking correctly.
+
 March 1, 2026
 * Fixed issues with icons/badges not
 showing properly on the map. Will be 
