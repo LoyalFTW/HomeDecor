d2c505ce610903309ada8f38e42c415f9b829b6e
Azro <47503034+LoyalFTW@users.noreply.github.com>
2026-03-05 08:35:32 -0400

    Revise changelog for March 2026 updates
    Updated changelog dates and removed outdated event information.

--- Changelog.lua
+++ Changelog.lua
@@ -1,15 +1,10 @@
 HomeDecor_Changelog = [[
 #HomeDecor
 
-March 2, 2026
+March 5 2026
 
-* Sales panel had an error not showing 
-the correct Id it now works and is tracking correctly.
-
-March 1, 2026
-* Fixed issues with icons/badges not
-showing properly on the map. Will be 
-looking into any vendors that are messed up.
+* Removed the events since there isn't any going on
+will update with new stuff soon. 
 
   
 ]]
