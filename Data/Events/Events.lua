3b175926f2924c692b9658a798c024e26bc0c8cf
Azro <47503034+LoyalFTW@users.noreply.github.com>
2026-03-05 08:33:47 -0400

    Update Events.lua

--- Data/Events/Events.lua
+++ Data/Events/Events.lua
@@ -6,27 +6,4 @@ NS.Data.Events = NS.Data.Events or {}
 NS.Data.Events["Events"] = NS.Data.Events["Events"] or {}
 NS.Data.Events["Events"]["New Events"] = {
 
-  {
-    title="Materialist Ophinell",
-    endsOn="Mar-02",
-    source={
-      id=249196,
-      type="vendor",
-      faction="Neutral",
-      zone="Twilight Highlands",
-      worldmap="241:4960:8120"},
-    items={
-      {decorID=714,       source={type="vendor",  itemID=245284, currency="2000", currencytype="Honor"}},
-      {decorID=1236,  source={type="vendor",  itemID=245330, currency="2000", currencytype="Honor"}},
-      {decorID=1227,       source={type="vendor",  itemID=251997, currency="2000", currencytype="Honor"}}}
-  },
-   {
-   title="It's Nearly Midnight",
-   endsOn="Mar-02",
-   source={
-     type="external",
-     platform="twitch",
-     label="These can be bought in Stormwind and Org",
-     url="https://twitch.tv/"},
-   items={
-     {decorID=14467,  source={type="external", itemID=260785}}}}}
+}
