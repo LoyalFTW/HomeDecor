fc63b1df622f6caead04686777f9e5814c6ad8ad
Azro <47503034+LoyalFTW@users.noreply.github.com>
2026-03-05 20:30:01 -0400

    Update changelog date and add bug fix
    Updated the changelog date and added a bug fix entry.

--- Changelog.lua
+++ Changelog.lua
@@ -1,7 +1,11 @@
 HomeDecor_Changelog = [[
 #HomeDecor
 
-March 5 2026
+March 6, 2026
+
+* Fixed a bug with Endeavors
+
+March 5, 2026
 
 * Removed the events since there isn't any going on
 will update with new stuff soon. 
